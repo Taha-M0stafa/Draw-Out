a2a5439e00527e24f6bcb9bec1bf71e0474a94ab
Taha Mostafa <20231700229@cis.asu.edu.eg>
2025-09-02 02:09:59 +0300

    added CharachterMovement

--- My project/Assets/Scripts/charMovement.cs
+++ My project/Assets/Scripts/charMovement.cs
@@ -1,32 +1,89 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using System.Collections;
 
 public class charMovement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    CharacterController controller;
+    private CharacterController controller;
+    private InputAction moveAction;
+    private InputAction jumpAction;
+    private const float SPEED = 10f;
+    private const float SPRINT_FACTOR = 2f;
+
+    private bool _canDash = true;
+    private bool _isDashing = false;
+    public float dashTime = 1f;
+    public float dashPower = 15f;
+    private Vector2 dashDirection;
+
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        moveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
     }
 
     // Update is called once per frame
     void Update()
     {
-        controller.Move(new Vector3(1, 0, 0) * Time.deltaTime);
-    }
+        Vector2 moveValue = moveAction.ReadValue<Vector2>();
+        bool isSprinting = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
 
-
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Enemy"))
+        if (!_isDashing)
         {
-            Debug.Log("Collided with Enemy");
+            if (isSprinting)
+            {
+                controller.Move(moveValue * (SPEED * Time.deltaTime * SPRINT_FACTOR));
+            }
+            else
+            {
+                controller.Move(moveValue * (Time.deltaTime * SPEED));
+            }
+
+            
+            if (moveValue != Vector2.zero)
+            {
+                dashDirection = moveValue;
+            }
+            
+            if (Input.GetKeyDown(KeyCode.Space) && _canDash)
+            {
+                IEnumerator coroutine = DashCoroutine(dashDirection);
+                
+                StartCoroutine(coroutine);
+            }
         }
     }
 
 
+    private IEnumerator DashCoroutine(Vector2 moveValue)
+    {
+        float startTime = Time.time;
+        Debug.Log("JUst outside the if condition!!!!!");
+        Debug.Log("About to dash oh boy!!");
+        _isDashing = true;
+        while (Time.time < startTime + dashTime)
+        {
+            controller.Move(moveValue * (dashPower * Time.deltaTime));
+            Debug.Log("Dashing!");
+            yield return null;
+        }
+
+        _canDash = false;
+        _isDashing = false;
+        yield return RefreshDash();
+    }
+
+    private IEnumerator RefreshDash()
+    {
+        yield return new WaitForSeconds(0.5f);
+        _canDash = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+    }
 }
-
-
-
