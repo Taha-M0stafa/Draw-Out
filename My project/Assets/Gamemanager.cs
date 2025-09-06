91c4a3144e26eebbfab51e1e215392f23f0e833a
simplescholar1 <mohamedytaha@aucegypt.edu>
2025-09-06 19:01:25 +0300

    gamemanager and portals

--- My project/Assets/Gamemanager.cs
+++ My project/Assets/Gamemanager.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,15 +6,25 @@ public class Gamemanager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public static Gamemanager instance;
-    [SerializeField ] string [] LevelNames;
+    [SerializeField] string[] LevelNames;
+
+    public GameObject Portalspawnpoint;
+    [SerializeField] GameObject Portal;
+    public int Enemycount; 
 
 
+    void OnSceneloaded(Scene scene, LoadSceneMode mode)
+    {
 
 
-
-
+        Getportalspawnpoint();
+        GetEnemycount();
+    }
     private void Awake()
     {
+
+
+
         if (instance == null)
         {
             instance = this;
@@ -25,29 +36,54 @@ public class Gamemanager : MonoBehaviour
         }
     }
 
-    void Loadlevel()
+    public void Loadlevel()
     {
-        int RandomLevel = Random.Range(0, LevelNames.Length);
-        string NextLevel = LevelNames[RandomLevel];
-        while (NextLevel == SceneManager.GetActiveScene().name)
+        int Randomlevel = Random.Range(0, LevelNames.Length);
+        string Nextlevel = LevelNames[Randomlevel];
+        while (Nextlevel == SceneManager.GetActiveScene().name)
         {
-            RandomLevel = Random.Range(0, LevelNames.Length);
-            NextLevel = LevelNames[RandomLevel];
+            Randomlevel = Random.Range(0, LevelNames.Length);
+            Nextlevel = LevelNames[Randomlevel];
 
 
 
         }
-        SceneManager.LoadScene(NextLevel);
+        SceneManager.LoadScene(Nextlevel);
     }
 
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+    public void EnemyDied()
+    {
+        Enemycount--;
+        if (Enemycount <= 0)
+        {
+            Instantiate(Portal, Portalspawnpoint.transform.position, Quaternion.identity);
+        }
+    }
+    void Getportalspawnpoint()
+    {
+        Portalspawnpoint = GameObject.FindGameObjectWithTag("Portalspawnpoint");
+    }
+    void GetEnemycount()
+    {
+        Enemycount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+    }
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneloaded;
+    }
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneloaded;
+    }
+
 }
