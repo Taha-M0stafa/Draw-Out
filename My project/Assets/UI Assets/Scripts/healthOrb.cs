f1ccd3ec50466ea04156ba5c1d21bbed50a0e433
Mohamed Hamza <mohamedelwan260@gmail.com>
2025-09-07 02:04:29 +0300

    Ui 02

---
+++ My project/Assets/UI Assets/Scripts/healthOrb.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthOrb : MonoBehaviour
+{
+    public Slider slider;
+    public void SetMaxHealth(float maxHealth)
+    {
+        slider.maxValue = maxHealth;
+        slider.value = maxHealth;
+    }
+    public void SetHealth(float health)
+    {
+        slider.value = health;
+    }
+}
